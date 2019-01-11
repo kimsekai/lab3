fd60e1f7b6dc3ec73b4d60b8aa42f9a29051025e
lab314-15@labeng.local <lab314-15@labeng.local>
2019-01-11 16:18:20 +0700

    lab3

---
+++ lab3_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 2;
+    double sum=0;
+    while(x<=69){
+        
+        sum = sum + (1.0/x);
+        x = x+1;
+
+    }
+    cout << sum;
+
+    return 0;
+}
